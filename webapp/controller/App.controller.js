4c25e389b9b356f68f005b18ff0e117ccdba0e50
DKSH_Fiori <KUL_CSSC_SAP_ABAP@dksh.com>
2023-07-04 10:29:13 +0000

    Push the code to Github

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ysd.wa.wrkflapp.YSD_WA_WRKFLAPP.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"ysd/wa/wrkflapp/YSD_WA_WRKFLAPP/test/unit/AllTests"
+	], function () {
+		QUnit.start();
+	});
+});
